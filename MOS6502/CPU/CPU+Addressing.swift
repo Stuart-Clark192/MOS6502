8b50ce898883c4292c5edd76197b7408aad2f4db
Stuart Clark <>
2020-11-16 08:17:02 +0000

    Add addressing mode extensions

---
+++ MOS6502/CPU/CPU+Addressing.swift
@@ -0,0 +1,18 @@
+//
+//  CPU+Addressing.swift
+//  MOS6502
+//
+//  Created by Stuart on 15/11/2020.
+//
+
+import Foundation
+
+extension CPU {
+    
+    
+    
+    func implied() -> UInt8 {
+        
+        return 0
+    }
+}
